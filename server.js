fb27cfc699dea63e56948b09297e815268d3ea3f
José Almeida <202100661@estudantes.ips.pt>
2025-08-24 19:05:48 +0100

    Prepare for Render deploy

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 // server.js
 const { serveHTTP } = require('stremio-addon-sdk');
-const addonInterface = require('./addon');
+const addonInterface = require('./addon'); // exports builder.getInterface()
 
 const PORT = process.env.PORT || 7000;
-serveHTTP(addonInterface, { port: PORT });
 
+serveHTTP(addonInterface, { port: PORT });
 console.log(`Stremio add-on running on http://127.0.0.1:${PORT}/manifest.json`);
+console.log(`HTTP addon accessible at: http://localhost:${PORT}/manifest.json`);
